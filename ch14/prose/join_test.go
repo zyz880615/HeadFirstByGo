4ad7c98579157f9853f561314c9bc8f3bcafc80c
zyz880615 <zyz880615@163.com>
2025-05-15 16:37:09 +0800

    提交第十四章代码

---
+++ ch14/prose/join_test.go
@@ -0,0 +1,30 @@
+package prose
+
+import (
+	"fmt"
+	"testing"
+)
+
+type testData struct {
+	list []string
+	want string
+}
+
+func TestJoinWithCommas(t *testing.T) {
+	tests := []testData{
+		{list: []string{}, want: ""},
+		{list: []string{"apple"}, want: "apple"},
+		{list: []string{"apple", "orange"}, want: "apple and orange"},
+		{list: []string{"apple", "orange", "pear"}, want: "apple, orange, and pear"},
+	}
+	for _, test := range tests {
+		got := JoinWithCommas(test.list)
+		if got != test.want {
+			t.Errorf(errorString(test.list, got, test.want))
+		}
+	}
+}
+
+func errorString(list []string, got string, want string) string {
+	return fmt.Sprintf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
+}
