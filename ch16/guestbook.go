23c7d997810dbf77a15a5394086600fb70fa6635
zyz880615 <zyz880615@163.com>
2025-05-15 22:54:24 +0800

    提交第十六章代码

---
+++ ch16/guestbook.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Guestbook struct {
+	SignatureCount int
+	Signatures     []string
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getStrings(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	if os.IsExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+}
+
+func newHandler(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("ch16/new.html")
+	check(err)
+	err = html.Execute(writer, nil)
+	check(err)
+}
+
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	signatures := getStrings("ch16/signatures.txt")
+	html, err := template.ParseFiles("ch16/view.html")
+	check(err)
+	guestbook := Guestbook{
+		SignatureCount: len(signatures),
+		Signatures:     signatures,
+	}
+	err = html.Execute(writer, guestbook)
+	check(err)
+}
+
+func createHandler(writer http.ResponseWriter, request *http.Request) {
+	signature := request.FormValue("signature")
+	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	file, err := os.OpenFile("ch16/signatures.txt", options, os.FileMode(0600))
+	check(err)
+	_, err = fmt.Fprintln(file, signature)
+	check(err)
+	err = file.Close()
+	check(err)
+	//重定向到"/guestbook"页面
+	http.Redirect(writer, request, "/guestbook", http.StatusFound)
+}
+
+func main() {
+	http.HandleFunc("/guestbook", viewHandler)
+	http.HandleFunc("/guestbook/new", newHandler)
+	http.HandleFunc("/guestbook/create", createHandler)
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
+}
